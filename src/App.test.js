a71f68e9dbf320550a1ea0091f42231eb66b65fb
simolex <simolex@yandex.ru>
2022-05-04 15:04:21 +0300

    проверка синхронного и ассинхроннрго контента

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("TEST App", () => {
+  test("renders learn react link", () => {
+    render(<App />);
+    const helloWorldElem = screen.getByText(/hello world/i);
+    const btnElem = screen.getByRole("button");
+    const inputElem = screen.getByPlaceholderText(/input value/i);
+    expect(helloWorldElem).toBeInTheDocument();
+    expect(btnElem).toBeInTheDocument();
+    expect(inputElem).toMatchSnapshot();
+  });
+
+  test("renders learn react link", async () => {
+    render(<App />);
+    // const helloWorldElem = screen.queryByText(/hello2/i);
+    // expect(helloWorldElem).toBeNull();
+    const helloWorldElem = await screen.findByText(/data/i);
+    expect(helloWorldElem).toBeInTheDocument();
+    expect(helloWorldElem).toHaveStyle({ color: "red" });
+  });
 });
